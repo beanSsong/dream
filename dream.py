2c80b90fa45e739da08de83646e16b4cf78029f5
Richard C Gerkin <rgerkin@asu.edu>
2015-02-06 01:27:57 -0700

    Updated code to support production of submittable files

--- dream.py
+++ dream.py
@@ -1,5 +1,6 @@
 import csv
 import ast
+import numpy as np
 
 def load_perceptual_data():
     data = []
@@ -9,7 +10,7 @@ def load_perceptual_data():
             if line_num > 0:
                 line[0:6] = [x.strip() for x in line[0:6]]
                 line[2] = True if line[2]=='replicate' else False
-                line[6:] = [9999 if x=='NaN' else int(x) for x in line[6:]]
+                line[6:] = ['NaN' if x=='NaN' else int(x) for x in line[6:]]
                 data.append(line)
             else:
                 headers = line
@@ -21,8 +22,24 @@ def load_molecular_data():
         reader = csv.reader(f, delimiter="\t")
         for line_num,line in enumerate(reader):
             if line_num > 0:
-                line[1:] = [9999 if x=='NaN' else float(x) for x in line[6:]]
+                line[1:] = ['NaN' if x=='NaN' else float(x) for x in line[6:]]
                 data.append(line)
             else:
                 headers = line
     return headers,data
+
+def leaderboard_CIDs():
+    """Return CIDs for molecules that will be used for the leaderboard
+    to determine the provisional leaders of the competition."""
+    with open('CID_leaderboard.txt') as f:
+        reader = csv.reader(f)
+        result = [int(line[0]) for line in reader]
+    return result
+
+def testset_CIDs():
+    """Return CIDs for molecules that will be used for final testing
+    to determine the winners of the competition."""
+    with open('CID_testset.txt') as f:
+        reader = csv.reader(f)
+        result = [int(line[0]) for line in reader]
+    return result
